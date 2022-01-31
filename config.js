82137c5659df2b7e194b02371a13e662a8c34c1b
Rishi293 <77837437+Rishi293@users.noreply.github.com>
2022-01-31 20:42:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+     apiKey: "AIzaSyCe5a0oQu-29jeXlTu5hz9-sBXnQxGaepI",
+    authDomain: "spectagram-stage-2.firebaseapp.com",
+    databaseURL: "https://spectagram-stage-2-default-rtdb.firebaseio.com",
+    projectId: "spectagram-stage-2",
+    storageBucket: "spectagram-stage-2.appspot.com",
+    messagingSenderId: "299953530534",
+    appId: "1:299953530534:web:ee403a9fab211869583667"
+};
